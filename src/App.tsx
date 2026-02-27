dda4c70dc54b60979e5da0b1b3d60ea23f40c88f
김문성 <k.ms@astkorea.net>
2026-02-27 14:18:34 +0900

    게시판 메뉴 하위 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import NoticePage from './pages/board/NoticePage.tsx';
 import './tailwind.css';
 import ApprovalPage from "@/pages/ApprovalPage.tsx";
 import Layout from "@/components/Layout.tsx";
+import WorkSharePage from "@/pages/board/WorkSharePage.tsx";
+import PolicyPage from "@/pages/board/PolicyPage.tsx";
+import EventPage from "@/pages/board/EventPage.tsx";
+import PeoplePage from "@/pages/board/PeoplePage.tsx";
+import EducationPage from "@/pages/board/EducationPage.tsx";
+import ClubPage from "@/pages/board/ClubPage.tsx";
+import IdeaPage from "@/pages/board/IdeaPage.tsx";
+import FreePage from "@/pages/board/FreePage.tsx";
 
 function App() {
   return (
@@ -16,14 +24,14 @@ function App() {
 
             <Route path="/board" element={<NoticePage />} />
             <Route path="/board/notice" element={<NoticePage />} />
-            <Route path="/board/work-share" element={<NoticePage />} />
-            <Route path="/board/policy" element={<NoticePage />} />
-            <Route path="/board/event" element={<NoticePage />} />
-            <Route path="/board/people" element={<NoticePage />} />
-            <Route path="/board/education" element={<NoticePage />} />
-            <Route path="/board/club" element={<NoticePage />} />
-            <Route path="/board/idea" element={<NoticePage />} />
-            <Route path="/board/free" element={<NoticePage />} />
+            <Route path="/board/work-share" element={<WorkSharePage />} />
+            <Route path="/board/policy" element={<PolicyPage />} />
+            <Route path="/board/event" element={<EventPage />} />
+            <Route path="/board/people" element={<PeoplePage />} />
+            <Route path="/board/education" element={<EducationPage />} />
+            <Route path="/board/club" element={<ClubPage />} />
+            <Route path="/board/idea" element={<IdeaPage />} />
+            <Route path="/board/free" element={<FreePage />} />
             <Route path="/board/photo" element={<NoticePage />} />
 
             <Route path="/approval" element={<ApprovalPage />} />
