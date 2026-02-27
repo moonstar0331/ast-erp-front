9b9911dfbb2879b78341350ecf4c511e843b7c95
김문성 <k.ms@astkorea.net>
2026-02-27 16:55:22 +0900

    전체 메뉴별 페이지 중첩 라우팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,71 @@ function App() {
                 <Route path="photo" element={<NoticePage />} />
             </Route>
 
-            <Route path="/approval" element={<ApprovalPage />} />
+            {/* 이메일 관련 중첩 라우팅 */}
+            <Route path="/email">
+                <Route index element={<Navigate to="/email/all" replace />} />
+                <Route path="all" element={<NoticePage />} />
+                <Route path="sent" element={<NoticePage />} />
+                <Route path="spam" element={<NoticePage />} />
+                <Route path="trash" element={<NoticePage />} />
+                <Route path="setting" element={<NoticePage />} />
+            </Route>
+
+            {/* 일정관리 관련 중첩 라우팅 */}
+            <Route path="/schedule">
+                <Route index element={<Navigate to="/schedule/monthly" replace />} />
+                <Route path="monthly" element={<NoticePage />} />
+                <Route path="reserve" element={<NoticePage />} />
+                <Route path="group" element={<NoticePage />} />
+            </Route>
+
+            {/* 업무관리 관련 중첩 라우팅 */}
+            <Route path="/task">
+                <Route index element={<Navigate to="/task/status" replace />} />
+                <Route path="status" element={<NoticePage />} />
+                <Route path="chat" element={<NoticePage />} />
+                <Route path="memo" element={<NoticePage />} />
+                <Route path="my" element={<NoticePage />} />
+                <Route path="report" element={<NoticePage />} />
+                <Route path="report-status" element={<NoticePage />} />
+                <Route path="recent" element={<NoticePage />} />
+                <Route path="comments" element={<NoticePage />} />
+                <Route path="project" element={<NoticePage />} />
+                <Route path="voucher" element={<NoticePage />} />
+            </Route>
+
+            {/* 전자결재 관련 중첩 라우팅 */}
+            <Route path="/approval">
+                <Route index element={<Navigate to="/approval/status" replace />} />
+                <Route path="new" element={<NoticePage />} />
+                <Route path="status" element={<ApprovalPage />} />
+                <Route path="setting" element={<NoticePage />} />
+            </Route>
+
+            {/* 웹디스크 관련 중첩 라우팅 */}
+            <Route path="/disk">
+                <Route index element={<Navigate to="/disk/list" replace />} />
+                <Route path="list" element={<NoticePage />} />
+                <Route path="download" element={<NoticePage />} />
+            </Route>
+
+            {/* 메모장 관련 중첩 라우팅 */}
+            <Route path="/memo">
+                <Route index element={<Navigate to="/memo/add" replace />} />
+                <Route path="add" element={<NoticePage />} />
+                <Route path="undo" element={<NoticePage />} />
+            </Route>
+            
+            {/* 명함관리 관련 중첩 라우팅 */}
+            <Route path="/card">
+                <Route index element={<Navigate to="/card/list" replace />} />
+                <Route path="list" element={<NoticePage />} />
+                <Route path="company-public" element={<NoticePage />} />
+                <Route path="unclassified" element={<NoticePage />} />
+                <Route path="company-list" element={<NoticePage />} />
+                <Route path="group" element={<NoticePage />} />
+            </Route>
+
         </Route>
       </Routes>
     </>
